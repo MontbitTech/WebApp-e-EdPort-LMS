025d8caa93f8a7a946862143f91dc3187b8e8cc8
krtrthbilory <kritarth.manu.billore@montbit.tech>
2020-07-08 16:45:22 +0530

    staging seeders

---
+++ database/seeds/StagingSeeder.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class StagingSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run () {
+        $this->settingTableSeeds();
+        $this->adminTableSeeds();
+    }
+
+    private function settingTableSeeds () {
+        DB::table('tbl_settings')->insert([
+            [
+                'item'       => 'domain',
+                'value'      => 'schooltimes-s.ca',
+                'folder'     => null,
+                'permission' => 'D',
+            ],
+            [
+                'item'       => 'year',
+                'value'      => '1522',
+                'folder'     => null,
+                'permission' => 'DE',
+            ],
+            [
+                'item'       => 'mailfrom',
+                'value'      => 'noreply@schooltimes-s.ca',
+                'folder'     => null,
+                'permission' => 'D',
+            ],
+            [
+                'item'       => 'schoolname',
+                'value'      => 'DPS Chennai',
+                'folder'     => null,
+                'permission' => 'DE',
+            ],
+            [
+                'item'       => 'schoollogo',
+                'value'      => 'http://lms.schooltimes-s.ca/public/images/Delhi-Public-School-Kolar-Road-Bhopal.png',
+                'folder'     => 'images',
+                'permission' => 'DF',
+            ],
+            [
+                'item'       => 'schooladdress',
+                'value'      => 'DPS Bhopal',
+                'folder'     => null,
+                'permission' => 'DEN',
+            ],
+        ]);
+    }
+
+    private function adminTableSeeds () {
+        DB::table('tbl_admin')->insert([
+            'first_name' => 'Admin',
+            'last_name'  => 'Sys',
+            'email'      => 'developer@schooltimes-s.ca',
+            'password'   => '',
+            'phone'      => '7981921945',
+        ]);
+    }
+}
