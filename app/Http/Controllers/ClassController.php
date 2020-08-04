fae82f3392da4b3bf130323508a09ad1f5c14cb9
vikram700 <vs4110690@gmail.com>
2020-08-04 21:52:07 +0530

    class change classroom

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -28,7 +28,7 @@ class ClassController extends Controller
      *
      * @return void
      */
-    public function __construct ()
+    public function __construct()
     {
         //$this->middleware('auth');
         //  return Auth::guard('admin');
@@ -39,44 +39,42 @@ class ClassController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function list_class ()
+    public function list_class()
     {
         $studentClasses = StudentClass::get();
         $classes        = ClassSection::orderByRaw("CAST(class_name as UNSIGNED) ASC")->get();
-        $section        = ClassSection::orderBy('section_name','ASC')->get();
-        
+        $section        = ClassSection::orderBy('section_name', 'ASC')->get();
+
         return view('admin.class.list_class', compact('classes', 'section', 'studentClasses'));
     }
 
 
-     public function filterSubject (Request $request, StudentClass $StudentClass)
+    public function filterSubject(Request $request, StudentClass $StudentClass)
     {
         $rec = $StudentClass->newQuery();
 
-        if(!empty($request->txtSerachByClass=='all-class')){
+        if (!empty($request->txtSerachByClass == 'all-class')) {
             $getResult = $rec->get();
-            if($request->txtSerachBySection && $request->txtSerachBySection!='all-section'){
-                $getResult = $rec->where('section_name', $request->txtSerachBySection)->get();
-            }          
-        }
-        else if($request->txtSerachByClass && $request->txtSerachBySection == 'all-section' ){
-            $getResult = $rec->where('class_name', $request->txtSerachByClass)->get();
-        }
-        else if(!empty($request->txtSerachByClass) &&($request->txtSerachByClass!='all-class')){
-            $getResult = $rec->where('class_name', $request->txtSerachByClass)->get();
-            if(!empty($request->txtSerachBySection && $request->txtSerachBySection != 'all-section' )){
+            if ($request->txtSerachBySection && $request->txtSerachBySection != 'all-section') {
                 $getResult = $rec->where('section_name', $request->txtSerachBySection)->get();
             }
-            if(!empty($request->txtSerachBySection && $request->txtSerachBySection == 'all-section' )){
+        } else if ($request->txtSerachByClass && $request->txtSerachBySection == 'all-section') {
+            $getResult = $rec->where('class_name', $request->txtSerachByClass)->get();
+        } else if (!empty($request->txtSerachByClass) && ($request->txtSerachByClass != 'all-class')) {
+            $getResult = $rec->where('class_name', $request->txtSerachByClass)->get();
+            if (!empty($request->txtSerachBySection && $request->txtSerachBySection != 'all-section')) {
+                $getResult = $rec->where('section_name', $request->txtSerachBySection)->get();
+            }
+            if (!empty($request->txtSerachBySection && $request->txtSerachBySection == 'all-section')) {
                 $getResult = $rec->where('class_name', $request->txtSerachBySection)->get();
             }
         }
         return view('admin.class.filter-subject', compact('getResult'));
     }
 
-    public function addClasses (Request $request)
+    public function addClasses(Request $request)
     {
-        if ( $request->isMethod('post') ) {
+        if ($request->isMethod('post')) {
             $request->validate([
                 'class_name' => 'required|max:100',
                 'subject'    => 'required|array|min:1',
@@ -87,9 +85,9 @@ class ClassController extends Controller
 
             $classExist = StudentClass::with('studentSubject')->where('class_name', $request->class_name)->where('section_name', $request->section)->whereIn('subject_id', $request->subject)->get();
 
-            if ( count($classExist) ) {
+            if (count($classExist)) {
                 $errorMsg = 'Classes with ';
-                foreach ( $classExist as $class ) {
+                foreach ($classExist as $class) {
                     $errorMsg .= $class->studentSubject->subject_name . ' ';
                 }
 
@@ -97,7 +95,7 @@ class ClassController extends Controller
             }
 
             $successMessage = 'Classes with ';
-            foreach ( $subjects as $subject ) {
+            foreach ($subjects as $subject) {
 
                 $data = array(
                     "name"               => $request->class_name . ' ' . $subject->subject_name,
@@ -115,19 +113,19 @@ class ClassController extends Controller
 
                 $response = CommonHelper::create_class($token, $data); // access Google api craete Cource
 
-                if ( !$response['success'] ) {
-                    if ( $response['data']->status == 'UNAUTHENTICATED' ) {
+                if (!$response['success']) {
+                    if ($response['data']->status == 'UNAUTHENTICATED') {
                         Log::error($response['data']->message);
                         CustomHelper::get_refresh_token();
                         $token = CommonHelper::varify_Admintoken(); // verify admin token
 
                         $response = CommonHelper::create_class($token, $data); // access Google api craete Cource
-//                        return redirect()->route('admin.logout');
+                        //                        return redirect()->route('admin.logout');
                     }
                 }
 
-                if ( !$response['success'] ) {
-                    if ( $response['data']->status == 'UNAUTHENTICATED' ) {
+                if (!$response['success']) {
+                    if ($response['data']->status == 'UNAUTHENTICATED') {
                         Log::error($response['data']->message);
                         return redirect()->route('admin.logout');
                     }
@@ -156,30 +154,30 @@ class ClassController extends Controller
         //$data['subject']->prepend('Select Subject', '');
         $data['section'] = DB::table('tbl_classes')->select('section_name')->distinct()->get()->pluck('section_name', 'section_name');
         $data['class'] = DB::table('tbl_classes')->select('class_name')->distinct()->get()->pluck('class_name', 'class_name');
-        $data['class']->prepend('Select Class', '');
+        $data['class']->prepend('Select Division', '');
         $data['section']->prepend('Select Section', '');
 
         return view('admin.class.add', compact('data', $data));
     }
 
-    public function deleteClasses (Request $request)
+    public function deleteClasses(Request $request)
     {
-        if ( strtolower($request->delete) != 'delete' )
+        if (strtolower($request->delete) != 'delete')
             return redirect()->back()->with('error', "Type delete to confirm");
 
-        if ( $request->txt_class_id == '' )
+        if ($request->txt_class_id == '')
             return redirect()->route('admin.listClass');
 
         $classTimingExist = ClassTiming::where('class_id', $request->txt_class_id)->first();
-        if ( $classTimingExist )
+        if ($classTimingExist)
             return redirect()->route('admin.listClass')->with('error', "you cannot delete this class! it's associated with Teacher,Assignent....");
 
         $dateClassExist = DateClass::where('class_id', $request->txt_class_id)->first();
-        if ( $dateClassExist )
+        if ($dateClassExist)
             return redirect()->route('admin.listClass')->with('error', "you cannot delete this class! it's associated with Teacher,Assignent....");
 
         $classWorkExits = ClassWork::where('class_id', $request->txt_class_id)->first();
-        if ( $classWorkExits )
+        if ($classWorkExits)
             return redirect()->route('admin.listClass')->with('error', "you cannot delete this class! it's associated with Assignent....");
 
         $classes = StudentClass::find($request->txt_class_id);
@@ -187,8 +185,8 @@ class ClassController extends Controller
         $token = CommonHelper::varify_Admintoken(); // verify admin token
 
         $response = CommonHelper::delete_class($token, $classes->g_class_id); // access Google api delete Cource
-        if ( !$response['success'] ) {
-            if ( $response['data']->status == 'UNAUTHENTICATED' )
+        if (!$response['success']) {
+            if ($response['data']->status == 'UNAUTHENTICATED')
                 return redirect()->route('admin.logout');
 
             return back()->with('error', $response['message']->message);
