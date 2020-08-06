afecf73d691a25addf2d2ccdfac3d5c36d320a88
krtrthbilory <67820106+krtrthbilory@users.noreply.github.com>
2020-08-06 12:31:00 +0530

    datatable

--- resources/views/admin/help/list.blade.php
+++ resources/views/admin/help/list.blade.php
@@ -166,7 +166,7 @@
     </div>
     <script type="text/javascript">
         $(document).ready(function () {
-            $('#teacherlist').DataTable({
+            $('#ticketlist').DataTable({
                 initComplete: function (settings, json) {
                     $('[data-dtlist="#' + settings.nTable.id + '"').html($('#' + settings.nTable.id + '_length').find("label"));
                     $('[data-dtfilter="#' + settings.nTable.id + '"').html($('#' + settings.nTable.id + '_filter').find("input[type=search]").attr('placeholder', $('#' + settings.nTable.id).attr('data-filterplaceholder')))
