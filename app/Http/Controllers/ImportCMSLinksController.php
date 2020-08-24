63db8b7a417bd6837d64bdd3e1998b1b4d86ab70
krtrthbilory <67820106+krtrthbilory@users.noreply.github.com>
2020-08-24 13:35:05 +0530

    added chapter field in cms links

--- app/Http/Controllers/ImportCMSLinksController.php
+++ app/Http/Controllers/ImportCMSLinksController.php
@@ -30,6 +30,7 @@ class ImportCMSLinksController extends Controller
 
 			$request->validate([
 				'subject' => 'required',
+				'chapter' => 'required',
 				'class'   => 'required',
 				'topic'   => 'required',
 				'link'    => 'required_without_all:khan_academy,youtube,others,alink|nullable|regex:' . $regex,
@@ -40,8 +41,8 @@ class ImportCMSLinksController extends Controller
 			]);
 
 			$studentClassExist = \DB::select(
-				'select id from tbl_cmslinks where class=? and subject=? and topic=? and link=? and assignment_link=?',
-				[$request["class"], $request["subject"], $request["topic"], $request["link"], $request["alink"],]
+				'select id from tbl_cmslinks where class=? and subject=? and chapter=? and topic=? and link=? and assignment_link=?',
+				[$request["class"], $request["subject"], $request['chapter'], $request["topic"], $request["link"], $request["alink"],]
 			);
 
 			if ($studentClassExist) {
@@ -49,7 +50,7 @@ class ImportCMSLinksController extends Controller
 				$error = "found";
 			} else
 				$s = \DB::table('tbl_cmslinks')->insert(
-					['class' => $request["class"], 'subject' => $request["subject"], 'topic' => $request["topic"], 'link' => $request["link"], 'khan_academy' => $request["khan_academy"], 'youtube' => $request["youtube"], 'others' => $request["others"], 'assignment_link' => $request["alink"]]
+					['class' => $request["class"], 'subject' => $request["subject"], 'chapter' => $request['chapter'], 'topic' => $request["topic"], 'link' => $request["link"], 'khan_academy' => $request["khan_academy"], 'youtube' => $request["youtube"], 'others' => $request["others"], 'assignment_link' => $request["alink"]]
 				);
 
 			if ($error == "found")
@@ -61,12 +62,11 @@ class ImportCMSLinksController extends Controller
 		$subjects = \DB::table('tbl_student_subjects')->get();
 		$data['class'] = \DB::table('tbl_classes')->select('class_name')->distinct()->get()->pluck('class_name', 'class_name');
 		$data['class']->prepend('Select Divison', '');
-
+		
 		return view('admin.cmslinks.add', compact('subjects', 'data'));
 	}
 	public function editLink(Request $request, $id)
 	{
-		# $regex  = '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/';  
 		$regex  = '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)([\/\w \.?]*)([\/\w \.=]*)*\/?$/';
 
 		if ($request->isMethod('post')) {
@@ -208,13 +208,13 @@ class ImportCMSLinksController extends Controller
 					$subjects = \DB::table('tbl_student_subjects')->where('subject_name', $reader["subject"])->get();
 
 					if (
-						!isset($reader["class"]) || !isset($reader["subject"]) || !isset($reader["topic"]) || !isset($reader["link"])
+						!isset($reader["class"]) || !isset($reader["subject"]) || !isset($reader["chapter"]) || !isset($reader["topic"]) || !isset($reader["link"])
 						|| !isset($reader["youtube"]) || !isset($reader["khan_academy"]) || !isset($reader["others"]) || !isset($reader["assignment"])
 					) {
 						return back()->with('error', 'Header Mismatch at row: ' . $i);
 					}
 
-					if ($reader["class"] == "" || $reader["subject"] == "" || $reader["topic"] == "") {
+					if ($reader["class"] == "" || $reader["subject"] == "" || $reader["chapter"] == "" || $reader["topic"] == "") {
 						Log::error('CMS details missing : ROW - ' . $i);
 						$error = "found";
 						$rows .= $i . ",";
@@ -263,7 +263,7 @@ class ImportCMSLinksController extends Controller
 							} else {
 								$s = \DB::table('tbl_cmslinks')->insert(
 									[
-										'class' => $reader["class"], 'subject' => $subjects[0]->id, 'topic' => $reader["topic"], 'link' => $reader["link"],
+										'class' => $reader["class"],'chapter' => $reader["chapter"], 'subject' => $subjects[0]->id, 'topic' => $reader["topic"], 'link' => $reader["link"],
 										'youtube' => $reader["youtube"], 'khan_academy' => $reader["khan_academy"], 'others' => $reader["others"],
 										'assignment_link' => $reader["assignment"]
 									]
