bd70034d7e7d33ab922238d445e3fc4f37c5abf8
krtrthbilory <67820106+krtrthbilory@users.noreply.github.com>
2020-08-27 17:48:52 +0530

    add default admin along with classroom creation

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,18 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\ClassSection;
-use Session;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    public function test (Request $request)
+    public function test (Request $request, $data=null)
     {
-       $class = ClassSection::firstOrCreate([
-            'class_name'   => 12,
-            'section_name' => 'C',
-        ]);
-        return \response()->json(['success' => 'true', 'message' => 'rest in peace bro...','data'=>$class]);
+        return \response()->json(['success' => 'true', 'message' => 'rest in peace bro...','data'=>$data]);
     }
 }
