e57df8168c736b5517d5a07c6743d188911919df
krtrthbilory <67820106+krtrthbilory@users.noreply.github.com>
2020-09-01 19:39:54 +0530

    weekley mails backend

---
+++ app/Http/Controllers/UtilityController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DateClass;
+use App\Http\Helpers\CustomHelper;
+use App\Models\ClassSection;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class UtilityController extends Controller
+{
+    public function weekleyMailsToStudents(Request $request)
+    {
+        set_time_limit(0);
+        $classes = ClassSection::with('students')->get();
+        $from = CustomHelper::getFromMail();
+
+        foreach($classes as $class){
+            $weeksClasses = DateClass::with('studentClass')->whereHas('studentClass',function($q) use($class){
+                $q->where('class_name', $class->class_name);
+                $q->where('section_name', $class->section_name);
+            })->get();
+
+            foreach($class->students as $student){
+                Mail::send('mail.weekleySchdule',  $weeksClasses, function ($message) use ($student, $from) {
+                    $message->to($student->email);
+                    $message->subject("This week's schdule");
+                    $message->from($from->value, 'MontBIt');
+                });
+            }
+        }
+
+        return $classes;
+    }
+}
