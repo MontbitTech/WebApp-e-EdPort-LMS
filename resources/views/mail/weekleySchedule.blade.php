696883c7fafb632cf6fc95620eb760ac8bccdf8c
krtrthbilory <67820106+krtrthbilory@users.noreply.github.com>
2020-09-07 18:58:36 +0530

    weekley schedule table borders

--- resources/views/mail/weekleySchedule.blade.php
+++ resources/views/mail/weekleySchedule.blade.php
@@ -1,25 +1,46 @@
-<h1>Hi,</h1>
-<h4>your this week's class schedule is as follows,</h4>
-<table>
-    <thead>
-        <tr>
-            <th>Date</th>
-            <th>Start</th>
-            <th>End</th>
-            <th>subject</th>
-            <th>Teacher</th>
-        </tr>
-    </thead>
-    <tbody>
-        
-        @foreach($weeksClasses as $weekclass)
-        <tr>
-            <td>{{$weekclass['class_date']}}</td>
-            <td>{{$weekclass['from_timing']}}</td>
-            <td>{{$weekclass['to_timing']}}</td>
-            <td>@if($weekclass['student_subject'] != null) {{$weekclass['student_subject']['subject_name']}} @endif</td>
-            <td>@if($weekclass['teacher'] != null) {{$weekclass['teacher']['name']}} @endif</td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+    <h1>Hi,</h1>
+    <h4>your this week's class schedule is as follows,</h4>
+    <table>
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>Start</th>
+                <th>End</th>
+                <th>subject</th>
+                <th>Teacher</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+            @foreach($weeksClasses as $weekclass)
+            <tr>
+                <td>{{$weekclass['class_date']}}</td>
+                <td>{{$weekclass['from_timing']}}</td>
+                <td>{{$weekclass['to_timing']}}</td>
+                <td>@if($weekclass['student_subject'] != null) {{$weekclass['student_subject']['subject_name']}} @endif</td>
+                <td>@if($weekclass['teacher'] != null) {{$weekclass['teacher']['name']}} @endif</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
